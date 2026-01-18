60ff011cabd33a4da986fd4248c6116058f7db59
Nicaredda <workcaredda1998@gmail.com>
2026-01-18 20:32:46 +0100

    .

--- script.js
+++ script.js
@@ -15,12 +15,8 @@ function divide(a, b){
     return total;
 };
 
-let operator = "";
-let fNumber = null;
-let sNumber = null;
-
+// we automate the creation of the ten buttons for the numbers, and stick them in the numbersContainer
 let nmbsContainer = document.querySelector("#numbersContainer");
-
 for (let n=0;n<10;n++){
     let btnNumbers = document.createElement("button");
     btnNumbers.className = "btnNumbers";
@@ -28,6 +24,20 @@ for (let n=0;n<10;n++){
     nmbsContainer.append(btnNumbers);
 }
 
+// for these ones we must use the input of a user
+let operator = "";
+let fNumber = null;
+let sNumber = null;
+
+function displayText(){
+    let display = document.querySelector("#display");
+    let text = document.createElement("h2");
+    text.id = "text"
+    text.textContent = "2 x 2";
+    display.append(text);
+}
 
 function calculator (){
 }
+
+displayText();
